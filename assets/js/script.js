bef67261a3581171173289e28f6bd6483b2c267e
Evan Werner <evan.g.werner@gmail.com>
2020-11-17 18:51:11 -0500

    added id's and proper row containers with classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-window.onload = function() {
-    var currentDate = moment();
-    var currentDayEl = $("#currentDay");
-    currentDayEl.innerHTML = currentDate.format('MMM DD, YYYY');
-};
+$().ready(function() {
+
+// Object value in hours to determine the current our for tasks
+
+// future, past and current block should be different colors
+
+// Save button should save data to localstorage and then hold after refresh
+
+
+    $(".saveBtn").click(function() {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+    });
+});
